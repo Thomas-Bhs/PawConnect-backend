27afddfc99147ecd80d95ec73575a7b5c72f008c
Aubry Gamard <aubry.soriano@gmail.com>
2025-12-12 15:20:11 +0400

    modif setPriority pour tout mettre en lowercase

--- modules/setPriority.js
+++ modules/setPriority.js
@@ -1,23 +1,24 @@
 // ALGO DE CALCUL DE PRIORITÉ EN FONCTION DES STATUS SIGNALÉS
 function setPriority(array = []) {
+  if (array.length === 0) return 'faible';
   const priorityValues = {
-    BLESSE: 50,
-    AFFAIBLI: 40,
-    DANGER: 50,
-    COINCE: 40,
-    PETITS: 30,
-    AGRESSIF: 20,
-    PEUREUX: 10,
-    JEUNE: 10,
-    SOCIABLE: 0,
-    SAIN: 0,
+    blesse: 50,
+    affaibli: 40,
+    danger: 50,
+    coince: 40,
+    petits: 30,
+    agressif: 20,
+    peureux: 10,
+    jeune: 10,
+    sociable: 0,
+    sain: 0,
   };
-  if (array.includes('BLESSE' || array.includes('DANGER'))) return 'URGENT';
+  if (array.includes('blesse' || array.includes('danger'))) return 'urgent';
   const score = array.reduce((total, state) => total + (priorityValues[state] || 0), 0);
-  if (score >= 80) return 'URGENT';
-  if (score >= 60) return 'IMPORTANT';
-  if (score >= 40) return 'MODERE';
-  return 'FAIBLE';
+  if (score >= 80) return 'urgent';
+  if (score >= 60) return 'important';
+  if (score >= 40) return 'modere';
+  return 'faible';
 }
 
 module.exports = { setPriority };
